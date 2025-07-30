52e457f08a49bead7bb2f96d7b18361b504e5aaa
Josegenese14 <jgenese6@gmail.com>
2025-07-29 21:41:01 -0300

    Product

---
+++ src/main/java/com/educandoweb/curso/services/ProductServices.java
@@ -0,0 +1,27 @@
+package com.educandoweb.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.curso.entities.Product;
+import com.educandoweb.curso.repositories.ProductRepository;
+
+@Service
+public class ProductServices {
+
+	@Autowired
+	private ProductRepository repository;
+
+	// pesquisa e retorna lista de produtos
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
